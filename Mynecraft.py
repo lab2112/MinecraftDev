eb1a908999c84fe7a5e577d0663ef7265d584c1b
Brandon <brandon.coates@gmail.com>
2015-03-19 03:51:43 +0000

    updated Myncraft

--- Mynecraft.py
+++ Mynecraft.py
@@ -1,7 +1,16 @@
 import mcpi.minecraft as minecraft
 import mcpi.block as block
+import random
 
 mc = minecraft.Minecraft.create()
 
-pos=mc.player.getTilePos()
+cur_pos=mc.player.getTilePos()
 
+play_game(cur_pos)
+
+def play_game(cur_pos):
+    cur_x = cur_pos.x
+    cur_z = cur_pos.z
+    new_x = random.randomint(cur_x, cur_x+5)
+    new_y = random.randomint(cur_y, cur_y+5)
+    
