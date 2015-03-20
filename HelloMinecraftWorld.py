95b535b12343c3a944bb1407753e1d5cf508f1a5
Brandon <brandon.coates@gmail.com>
2015-03-20 04:27:40 +0000

    Added comments to Hello file

--- HelloMinecraftWorld.py
+++ HelloMinecraftWorld.py
@@ -1,5 +1,8 @@
+#Bringing in other packages
 import mcpi.minecraft as minecraft
 
+#Open a connection to minecraft
 mc = minecraft.Minecraft.create()
 
+#Say Hello
 mc.postToChat("Hello Minecraft World")
